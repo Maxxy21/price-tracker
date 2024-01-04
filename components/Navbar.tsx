0b5ff2d54db663f9ea64d3a5b32ef88d42bb8662
Maxwell Aboagye <nanabaah12@gmail.com>
2024-01-04 22:35:15 +0100

    changed the title

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -20,7 +20,7 @@ const Navbar = () => {
                     />
 
                     <p className="nav-logo">
-                        Price<span className='text-primary'>Wise</span>
+                        Rabatt<span className='text-primary'>Radar</span>
                     </p>
                 </Link>
 
